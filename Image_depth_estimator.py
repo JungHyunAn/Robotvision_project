26892a24925fe48113fb43740bcbf07b0c84922f
JungHyunAn <49269870+JungHyunAn@users.noreply.github.com>
2024-10-05 20:15:45 +0900

    Delete Image_depth_estimator.py

--- Image_depth_estimator.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torchvision
-import mmcv
-import mmengine
-model = torch.hub.load('yvanyin/metric3d', 'metric3d_vit_small', pretrain=True)
-pred_depth, confidence, output_dict = model.inference({'input': rgb})
-pred_normal = output_dict['prediction_normal'][:, :3, :, :] # only available for Metric3Dv2 i.e., ViT models
-normal_confidence = output_dict['prediction_normal'][:, 3, :, :] # see https://arxiv.org/abs/2109.09881 for details
